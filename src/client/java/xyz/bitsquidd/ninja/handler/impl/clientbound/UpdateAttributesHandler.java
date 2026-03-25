a969022fe5653b13541c79dfdc751e5d972c8098
ImBit <151650938+ImBit@users.noreply.github.com>
2026-03-25 11:31:47 +0000

    ✨ Added UpdateAttributesHandler.

---
+++ src/client/java/xyz/bitsquidd/ninja/handler/impl/clientbound/UpdateAttributesHandler.java
@@ -0,0 +1,39 @@
+package xyz.bitsquidd.ninja.handler.impl.clientbound;
+
+import net.kyori.adventure.text.Component;
+import net.minecraft.network.protocol.game.ClientboundUpdateAttributesPacket;
+import org.jetbrains.annotations.NotNull;
+
+import xyz.bitsquidd.ninja.format.FormatHelper;
+import xyz.bitsquidd.ninja.format.PacketInfoBundle;
+import xyz.bitsquidd.ninja.format.PacketInfoSegment;
+import xyz.bitsquidd.ninja.handler.PacketHandler;
+import xyz.bitsquidd.ninja.handler.PacketType;
+
+import java.util.List;
+
+
+public class UpdateAttributesHandler extends PacketHandler<@NotNull ClientboundUpdateAttributesPacket> {
+
+    public UpdateAttributesHandler() {
+        super(
+              ClientboundUpdateAttributesPacket.class,
+              "UpdateAttributes",
+              "Changes a LivingEntity's attributes",
+              PacketType.CLIENTBOUND
+        );
+    }
+
+    @Override
+    protected @NotNull PacketInfoBundle getPacketInfoInternal(ClientboundUpdateAttributesPacket packet) {
+        return PacketInfoBundle.of(
+              packetType,
+              Component.text(friendlyName),
+              List.of(
+                    PacketInfoSegment.of(Component.text("EntityID"), Component.text(packet.getEntityId())),
+                    PacketInfoSegment.of(Component.text("Attributes"), Component.text(FormatHelper.formatList(packet.getValues().stream().map(FormatHelper::formatAttribute).toList(), 10)))
+              )
+        );
+    }
+
+}
